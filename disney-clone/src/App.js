3f61cb9e53b81e0f21b27f67911882cb96949fac
Grégory Néault <85392282+Gneo45@users.noreply.github.com>
2022-02-24 14:16:06 +0100

    ADD: login system

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -1,27 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import Detail from './components/Detail';
-import Login from './components/Login';
-import { BrowserRouter as Router,
-Routes,
-Route,
-Link
-} from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-     <Header/>
-    <Routes>
-    <Route path="/login" element={<Login />} />
-
-      <Route path="/" element={<Home />} />
-      <Route path="/detail/:id" element={<Detail />} />
-    </Routes>
+        <Header />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
       </Router>
     </div>
   );
--- disney-clone/src/components/Header.js
+++ disney-clone/src/components/Header.js
@@ -1,11 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { auth, provider } from "../firebase";
 import styled from "styled-components";
+import { Navigate, useNavigate } from 'react-router-dom'
+import { selectUserName, selectUserPhoto, setSignOut, setUserLogin } from "../features/user/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+
 
 function Header() {
+
+  const dispatch = useDispatch();
+  const history = useNavigate();
+const userName = useSelector(selectUserName);
+const userPhoto = useSelector(selectUserPhoto);
+
+useEffect(() => {
+  auth.onAuthStateChanged(async (user)=> {
+    if(user){
+      dispatch(setUserLogin({
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+      }))
+      history("/")
+    }
+  })
+}, [])
+
+
+const signIn = () => {
+  auth.signInWithPopup(provider)
+  .then((result)=>{
+    let user = result.user
+    dispatch(setUserLogin({
+      name: user.displayName,
+      email: user.email,
+      photo: user.photoURL,
+    }))
+    history("/")
+
+  })
+}
+
+const signOut = () => {
+  auth.signOut()
+  .then(() => {
+    dispatch(setSignOut());
+    history("/login")
+  })
+}
+
   return (
     <Nav>
       <Logo src="/images/logo.svg" />
-      <NavMenu>
+
+      {
+        !userName ? (
+          <LoginContainer>
+            <Login onClick={signIn}>Login</Login>
+            </LoginContainer>
+         ): 
+        <>
+         <NavMenu>
         <a>
           <img src="/images/home-icon.svg" />
           <span>HOME</span>
@@ -31,8 +86,12 @@ function Header() {
           <span>SERIES</span>
         </a>
       </NavMenu>
-      <UserImg src="/images/userImg.jpg"/>
-    </Nav>
+      <UserImg onClick={signOut} src="/images/userImg.jpg" />
+        </>
+        
+      }
+      </Nav>
+     
   );
 }
 
@@ -43,7 +102,6 @@ const Nav = styled.nav`
   align-items: center;
   padding: 0 36px;
   overflow-x: hidden;
-  
 `;
 
 const Logo = styled.img`
@@ -51,57 +109,78 @@ const Logo = styled.img`
 `;
 
 const NavMenu = styled.div`
-display: flex;
-flex: 1;
-margin-left: 20px;
-align-items: center;
+  display: flex;
+  flex: 1;
+  margin-left: 20px;
+  align-items: center;
 
-a{
-    display:flex;
-    align-items:center;
+  a {
+    display: flex;
+    align-items: center;
     padding: 0 12px;
     cursor: pointer;
-    
-    img{
-        height: 20px;
+
+    img {
+      height: 20px;
     }
 
-    span{
-        position: relative;
-        font-size: 13px;
-        letter-spacing: 1.42px;
+    span {
+      position: relative;
+      font-size: 13px;
+      letter-spacing: 1.42px;
 
-        &:after{
-            content: "";
-            height: 2px;                 
-            background: white;
-            position: absolute;           
-            left: 0;
-            right: 0;
-            bottom: -6px;
-            opacity: 0;
-            transform-origin: left center;
-            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-            transform: scaleX(0);
-           
-        }
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        transform: scaleX(0);
+      }
     }
 
     &:hover {
-        span:after{
-            transform: scaleX(1);
-            opacity: 1;
-           
-        }
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
     }
-}
+  }
 `;
 
 const UserImg = styled.img`
-width: 48px;
-height: 48px;
-border-radius: 50%;
-cursor: pointer;
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+const Login = styled.div`
+border: 1px solid #f9f9f9;
+padding: 8px 16px;
+border-radius: 4px;
+letter-spacing: 1.5px;
+text-transform: uppercase;
+background-color: rgba(0, 0, 0, 0.6);
+transition: all 0.2s ease 0s;
+cursor:pointer;
+
+&:hover{
+  color: black;
+  background-color: #f9f9f9;
+  border-color: transparent;
+}
+`
+
+const LoginContainer = styled.div`
+flex: 1;
+display: flex;
+justify-content: flex-end;
 `
 
 export default Header;
